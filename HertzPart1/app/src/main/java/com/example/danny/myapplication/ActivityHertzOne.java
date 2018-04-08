d2020fc78c7c19f1a2aae669a9d8e03c427b459b
DannyJSan <djsantos6730@eagle.fgcu.edu>
2018-04-08 01:34:21 -0400

    login/menu
    yup.

---
+++ HertzPart1/app/src/main/java/com/example/danny/myapplication/ActivityHertzOne.java
@@ -0,0 +1,27 @@
+package com.example.danny.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityHertzOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hertz_one);
+        configureEnter();
+    }
+
+    private void configureEnter() {
+        Button enterButton = (Button) findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ActivityHertzOne.this, SecActivity.class));
+            }
+        });
+    }
+}
